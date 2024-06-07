dfac2e8dec4e1d2b803d62ebaf74f560a559b949
Guilherme de Amorim <ggimenezjr@gmail.com>
2024-06-07 18:27:37 -0300

    chore: spacing for icon

--- config/.config/awesome/widgets/vpn.lua
+++ config/.config/awesome/widgets/vpn.lua
@@ -1,14 +1,14 @@
 local wibox = require("wibox")
 
 local vpnwidget = wibox.widget.textbox()
-vpnwidget:set_text("  ")
+vpnwidget:set_text("    ")
 local vpnwidgettimer = timer({ timeout = 5 })
 vpnwidgettimer:connect_signal("timeout", function()
 	local status = io.popen("pgrep openconnect", "r")
 	if status:read() == nil then
-		vpnwidget:set_markup("<span color='#FFFC00'></span> ")
+		vpnwidget:set_markup("<span color='#FFFC00'>    </span> ")
 	else
-		vpnwidget:set_markup("<span color='#FF0000'></span> ")
+		vpnwidget:set_markup("<span color='#FF0000'>    </span> ")
 	end
 	status:close()
 end)
